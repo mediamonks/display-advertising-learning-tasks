0930b59c10dea6a8acac74c27864f7f983954c43
Frankie <frankie.bukenya@gmail.com>
2021-07-05 00:18:08 +0200

    +(feat): Add bounce animation

---
+++ animation/bouncing_ball/index.js
@@ -0,0 +1,21 @@
+
+gsap.registerPlugin(CustomEase, CustomBounce);
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+    window.onload = function() {
+      
+      window.requestAnimationFrame(function () {
+        let duration = 1.5;
+        const tl = gsap.timeline();
+
+        //strength between 0 and 1
+        CustomBounce.create("footballBounce", {strength: 0.7, squash: 3});
+        tl.to("#football",  {duration, y: 335, ease:"footballBounce"})
+          .to("#football", { duration, scaleY: 0.5, scaleX: 1.3, ease: "footballBounce-squash", transformOrigin: "bottom" }, 0)
+          .to("#football", { duration, x: 600 }, '<')
+        
+        return tl;      
+       });  
+    };
+});
