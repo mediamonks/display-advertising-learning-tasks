87348c27010189bd38a2699e1b389706960821e7
Frankie <frankie.bukenya@gmail.com>
2021-07-31 18:31:02 +0200

    +(feat): Adjust bounce timing
    Twaeked the animation timuing of my custom bounce for better accuracy

--- animation/bouncing_ball/index.js
+++ animation/bouncing_ball/index.js
@@ -6,15 +6,16 @@ document.addEventListener("DOMContentLoaded", function() {
     window.onload = function() {
       
       window.requestAnimationFrame(function () {
-        let duration = 1.5;
+        let duration = 1.6;
         const tl = gsap.timeline();
 
-        CustomBounce.create("footballBounce", {strength: 0.7, squash: 3});
-        tl.to("#football",  {duration, y: 335, ease:"footballBounce"})
+        CustomBounce.create("footballBounce", { strength: 0.75, squash: 4});
+        tl.to("#football", { duration, y: 330, ease: "footballBounce" })
           .to("#football", { duration, scaleY: 0.5, scaleX: 1.3, ease: "footballBounce-squash", transformOrigin: "bottom" }, 0)
-          .to("#football", { duration, x: 600 }, '<')
+          .to("#football", { duration, x: 670 }, '<')
+
+        return tl;
         
-        return tl;      
        });  
     };
 });
