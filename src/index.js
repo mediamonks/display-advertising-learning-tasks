d91595697976485e49f9a50ba8a5e8cc8c97482a
Frankie <frankie.bukenya@gmail.com>
2021-08-26 09:19:02 +0200

    last commit

--- src/index.js
+++ src/index.js
@@ -5,21 +5,14 @@ window.onload = function () {
   const tl = gsap.timeline();
 
 
-        CustomBounce.create("footballBounce", { strength: 0.77, squash: 5 });
-        tl
-          .from('.content', { duration: 0.5, autoAlpha: 0 }) 
-          .to("#football", { duration: duration, y: 290, ease: "footballBounce" }, 'bounce')
-          .to("#football", { duration: duration, scaleY: 0.5, scaleX: 1.3, ease: "footballBounce-squash", transformOrigin: "bottom" }, 'bounce')
-          .to("#football", { duration: duration, y: 330, ease: "footballBounce" }, 'bounce')
-          .to("#football", { duration: duration, scaleY: 0.5, scaleX: 1.3, ease: "footballBounce-squash", transformOrigin: "bottom" }, 'bounce')
-          .to("#football", { duration: duration, y: 330, ease: "footballBounce" }, 'bounce')
-          .to("#football", { duration: duration, scaleY: 0.5, scaleX: 1.3, ease: "footballBounce-squash", transformOrigin: "bottom" }, 'bounce')
-          .to("#football", { duration: duration, y: 330, ease: "footballBounce" }, 'bounce')
-          .to("#football", { duration: duration, scaleY: 0.5, scaleX: 1.3, ease: "footballBounce-squash", transformOrigin: "bottom" }, 'bounce')
-
-
-          .to("#football", { duration: duration, x: 600 }, 'bounce')
-    
+  CustomBounce.create("footballBounce", { strength: 0.77, squash: 5 });
+  
+  tl
+    .set('#football', { top: -15,  left: 45})
+    .from('.content', { duration: 0.5, autoAlpha: 0 }) 
+    .to("#football", { duration: duration, y: 340, ease: "footballBounce" }, 'bounce')
+    .to("#football", { duration: duration, scaleY: 0.5, scaleX: 1.3, ease: "footballBounce-squash", transformOrigin: "bottom" }, 'bounce')
+    .to("#football", { duration: duration, x: 600 }, 'bounce')    
 
   GSDevTools.create();
   return tl;  
